f0c11c00adb27c4f09dcf9fb09161ff7d9700944
sravanireddy748 <kotlasravanireddy064@gmail.com>
2026-06-30 15:32:40 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,78 @@
+#store and display
+name="sravani"
+age=21
+height=5.7
+student=True
+print(name)
+print(age)
+print(height)
+
+
+
+# type identification
+
+a=100
+b=99.99
+c="Python"
+d=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+# integer operations
+
+a=58
+b=9
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+#float caluculations
+
+length=10.5
+width=5.2
+area=length*width
+perimeter=2+(length+width)
+print(length)
+print(width)
+print(area)
+print(perimeter)
+
+#task-5: string information
+
+name="sravani"
+print(name[0])
+print(name[-1])
+print(name.upper())
+print(len(name))
+
+# Task 8: User Information
+
+name=input("enter a name: ")
+age=int(input("enter your age: "))
+print("hello", name + ", you are", age, "years old")
+
+#Task 6: Boolean Check
+
+is_logged_in=False
+print("before: ", is_logged_in)
+is_logged_in=True
+print("after: ", is_logged_in)
+
+#Task 7: Type Conversion
+num_int=20
+num_float=float(num_int)
+print(num_float,type(num_float))
+price=66.90
+price_int=int(price)
+print(price_int,type(price_int))
+age=22
+age_str=str(age)
+print(age_str, type(age_str))
+text="500"
+text_int=int(text)
+print(text_int, type(text_int))
---
+++ operatoestask.py
@@ -0,0 +1,40 @@
+# 1.Take two numbers as input and perform:
+# Addition
+# Subtraction
+# Multiplication
+# Division
+# Modulus
+
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+# 2.Find whether a given number is even or odd using the modulus (%) operator.
+
+a=int(input("enter a number: "))
+if a % 2==0:
+    print("given number is even")
+else:
+    print("given number is odd")
+
+#3.Calculate the square and cube of a number using the exponent (**) operator.
+
+numb=7
+suare=numb**2
+cube=numb**3
+print(suare)
+print(cube)
+
+#4.Swap two numbers without using a third variable.
+a=4
+b=6
+a,b=b,a
+print(a)
+print(b)
+
+#5.Find the quotient and remainder when one number is divided by another.
+
