e88ce7de7dbf950534220025302e1e6cd80defac
sravanireddy748 <kotlasravanireddy064@gmail.com>
2026-07-06 21:33:56 +0530

    Add files via upload

---
+++ looping statement.py
@@ -0,0 +1,13 @@
+# print numbers 1-100,100-1,even numbers,and odd numbers
+
+for i in range(1,101,1):
+    print(i)
+
+for i in range(100,0,-1):
+    print(i)
+
+for i in range(0,101,2):
+    print(i)
+
+for i in range(1,101,2):
+    print(i)
