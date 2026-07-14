011dcb9afcba73e1ecc3f422002693b56a975230
sravanireddy748 <kotlasravanireddy064@gmail.com>
2026-07-14 20:25:35 +0530

    Add files via upload

---
+++ strings task 1.py
@@ -0,0 +1,63 @@
+# Given s = "PythonProgramming", slice the first 6 characters.
+# From s = "DataScience", extract "Science" using slicing.
+# Given s = "ArtificialIntelligence", get the last 12 characters.
+# From s = "MachineLearning", extract every second character.
+# Given s = "DeepLearning", reverse the string using slicing.
+
+# s="PythonProgramming"
+# print(s[0:6])
+
+# s = "DataScience"
+# print(s[4:])
+
+# s = "ArtificialIntelligence"
+# print(len(s))
+# print(s[10:])
+
+# s = "DeepLearning"
+# print(s[::-1])
+
+
+# Given s = "HelloWorld", extract "World" without using positive indices.
+# From s = "ComputerScience", remove the first and last character using slicing.
+# Given s = "InformationTechnology", extract "Technology" using negative indexing.
+# From s = "DataAnalysis", get characters from index 2 to 8 (inclusive).
+# Given s = "ProgrammingLanguage", extract characters at odd indices.
+
+# s = "HelloWorld"
+# print(s[-5::1])
+
+# s = "ComputerScience"
+# print(len(s))
+# print(s[1:14:1])
+
+# s = "InformationTechnology"
+# print(s[-10::1])
+
+# s = "DataAnalysis"
+# print(s[2:9])
+
+# s = "ProgrammingLanguage"
+# print(s[1::2])
+
+# Given s = "abcdefghij", reverse only the first 5 characters using slicing (result should be "edcbafghij").
+# From s = "1234567890", extract "24680" using slicing only.
+# Given s = "SlicingIsFun", swap the first and last 3 characters using slicing.
+# From s = "abcdefghijklmnop", extract every third character starting from index 1.
+# Given s = "Mississippi", extract "issi" using slicing in two different ways.
+
+# s = "SlicingIsFun"
+# print(s[-3:] + s[3:-3] + s[:3])
+
+# s = "abcdefghijklmnop"
+# print(s[1::3])
+
+# s = "Mississippi"
+# print(s[1:5])
+
+# s = "Mississippi"
+# print(s[-10:-6])
+
+
+
+
